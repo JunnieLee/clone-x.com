95463cf7c3292a36f63718ad7f182c79f173e662
JunnieLee <heunghari@naver.com>
2024-04-15 23:56:59 +0900

    fix: router.replace를 통해 클라이언트쪽에서 리다이렉트 -> 인터셉팅 라우트 정상작동 하도록 수정

--- src/app/(beforeLogin)/login/page.tsx
+++ src/app/(beforeLogin)/login/page.tsx
@@ -1,5 +1,10 @@
-import { redirect } from "next/navigation";
+"use client";
+
+import { useRouter } from "next/navigation";
+import Home from "@/app/(beforeLogin)/page";
 
 export default function Login() {
-  redirect("/i/flow/login");
+  const router = useRouter();
+  router.replace("i/flow/login"); // client쪽에서 리다이렉트
+  return <Home />;
 }
