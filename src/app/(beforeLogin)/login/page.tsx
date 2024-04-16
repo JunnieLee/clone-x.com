9263aac2e20b44d5877372627c7f1876e899d672
JunnieLee <heunghari@naver.com>
2024-04-16 17:17:19 +0900

    chore: 공통된 화면 따로 css로 뺌

--- src/app/(beforeLogin)/login/page.tsx
+++ src/app/(beforeLogin)/login/page.tsx
@@ -1,10 +1,10 @@
 "use client";
 
 import { useRouter } from "next/navigation";
-import Home from "@/app/(beforeLogin)/page";
+import Main from "../_component/Main";
 
 export default function Login() {
   const router = useRouter();
   router.replace("i/flow/login"); // client쪽에서 리다이렉트
-  return <Home />;
+  return <Main />;
 }
